ab9276ec23659dca321c80d3d75393958c67dc01
zafark1998 <138758513+zafark1998@users.noreply.github.com>
2023-07-06 13:01:39 +0500

    Add files via upload

---
+++ zainlist.py
@@ -0,0 +1,34 @@
+person = {'name1':'zain','name2':'faraz','name3':'ali'}
+
+person['name4'] = "hasnat"
+
+# print(person)
+
+person['name5'] = "ali"
+person['name6'] = "sameer"
+
+# print(person)
+
+del person['name3']
+
+# print(person)
+
+person['name5'] = "sarim"
+
+# print(person)
+
+# key =[]
+# value = []
+
+# for k,v in person.items():
+#     person.
+
+fruits = ["apple","banana","mango","strawberry","pineapple"]
+
+# print(fruits[0])
+
+for item in person:
+
+    print(item)
+
+# print(person['name2'])
